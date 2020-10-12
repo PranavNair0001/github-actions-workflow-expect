e18f653bc265948be3c0075e25a03086e3085372
Danil Vahidov <vahidov@gmail.com>
2020-10-12 14:34:34 +0200

    fix waitTime && misc fixes

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const STATUS_SUCCESS = 'success';
 const STATUS_ERROR = 'error';
 
 try {
-    const waitInterval = core.getInput('wait-interval');
-    const waitMax = core.getInput('wait-max');
+    const waitInterval = parseInt(core.getInput('wait-interval'));
+    const waitMax = parseInt(core.getInput('wait-max'));
     const ref = core.getInput('ref', {required: false}) || context.sha;
     const token = core.getInput('repo-token');
     let breakIfNoWork = false;
+    const BreakException = {};
     switch (core.getInput('no-work-break').toLowerCase().trim()) {
         case "true": case "yes": case "1": breakIfNoWork = true;
     }
@@ -26,20 +27,27 @@ try {
             return [STATUS_NO_WORK, null];
         }
         let status = STATUS_SUCCESS;
-        let objectWating = null;
-        response.check_runs.forEach(checkRun => {
-            if (checkRun.name !== context.job) {
-                objectWating = checkRun.name;
-                if (checkRun.status === 'in_progress' || checkRun.status === 'queued') {
-                    status = STATUS_IN_PROGRESS;
+        let jobName = null;
+        try {
+            response.check_runs.forEach(checkRun => {
+                if (checkRun.name !== context.job) {
+                    if (checkRun.status === 'in_progress' || checkRun.status === 'queued') {
+                        jobName = checkRun.name;
+                        status = STATUS_IN_PROGRESS;
+                        throw BreakException;
+                    }
+                    if (['failure', 'cancelled', 'timed_out'].includes(checkRun.conclusion)) {
+                        jobName = checkRun.name;
+                        status = STATUS_ERROR;
+                        throw BreakException;
+                    }
                 }
-                if (['failure', 'cancelled', 'timed_out'].includes(checkRun.conclusion)) {
-                    status = STATUS_ERROR;
-                    objectWating = checkRun.name;
-                }
-            }
-        })
-        return [status, objectWating];
+            })
+        } catch (e) {
+            if (e !== BreakException) throw e;
+        }
+
+        return [status, jobName];
     };
 
     const sleep = async function (seconds) {
@@ -48,12 +56,12 @@ try {
 
     (async () => {
         try {
-            console.log(`Starting checks for: ${owner} / ${repo} / ${ref}`);
+            console.log(`Starting checks for: ${owner} / ${repo} / ${ref}. Every ${waitInterval}s. out of ${waitMax}s.`);
             let executedTime = 0;
             let result = '';
-            let object = null;
+            let jobName = null;
             while (result !== STATUS_SUCCESS) {
-                [result, object] = await queryCheckWorkflows();
+                [result, jobName] = await queryCheckWorkflows();
                 if (result === STATUS_NO_WORK) {
                     if (breakIfNoWork === true) {
                         core.setFailed(`There aren't checks for this ref, exiting...`);
@@ -62,20 +70,20 @@ try {
                     }
                     break;
                 } else if (result === STATUS_ERROR) {
-                    core.setFailed(`Workflow ${object} ended with error, exiting...`);
+                    core.setFailed(`Workflow ${jobName} ended with error, exiting...`);
+                    break;
+                } else if (result === STATUS_SUCCESS) {
                     break;
                 }
                 if (executedTime > waitMax) {
                     core.setFailed('Time exceed max limit (' + waitMax + '), stopping...');
                     break;
                 }
-                console.log(`Workflow ${object} still in progress, will check for ${waitInterval} seconds...`);
+                console.log(`Workflow ${jobName} still in progress (${executedTime}s.), will check for ${waitInterval} seconds...`);
                 await sleep(waitInterval);
                 executedTime += waitInterval;
             }
-            if (result === STATUS_SUCCESS) {
-                console.log('All checks were passed.');
-            }
+            console.log('All checks were passed.');
         } catch (error) {
             core.setFailed(error.message);
         }
